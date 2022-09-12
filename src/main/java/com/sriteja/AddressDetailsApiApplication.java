12dde17541494f66dd444d02802f51e2a87ec607
ramesh cheppali <ramesh96420@gmail.com>
2022-09-12 20:22:03 +0530

    new adddree project crating

---
+++ src/main/java/com/sriteja/AddressDetailsApiApplication.java
@@ -0,0 +1,13 @@
+package com.sriteja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AddressDetailsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AddressDetailsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sriteja/AddressDetailsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sriteja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AddressDetailsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
